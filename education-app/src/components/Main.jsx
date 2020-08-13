05744d189820adbfd6fb667b2aa36bb9224ff1a8
Nathan Sou <nathanksou@gmail.com>
2020-08-12 20:27:17 -0700

    Implement Header and Body components of Main Page

--- education-app/src/components/Main.jsx
+++ education-app/src/components/Main.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { MainPage, MainHeader, MainBody } from './styled-components/MainComponents';
 
 const Main = (props) => {
 
-
   return (
-    <div>{props.name}</div>
+    <MainPage>
+      <MainHeader>
+        <div>{props.name}</div>
+      </MainHeader>
+      <MainBody>
+        <div>Body</div>
+      </MainBody>
+    </MainPage>
   );
 };
 
--- education-app/src/components/styled-components/MainComponents.jsx
+++ education-app/src/components/styled-components/MainComponents.jsx
@@ -1,1 +1,31 @@
 import styled from 'styled-components';
+
+export const MainPage = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #fff;
+  color: black;
+  font-size: calc(10px + 2vmin);
+`;
+
+export const MainHeader = styled.div`
+  width: 100%;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background-color: #aaa;
+`;
+
+export const MainBody = styled.div`
+  width: 100%;
+  flex: 4;
+  background-color: #ccc;
+`;
