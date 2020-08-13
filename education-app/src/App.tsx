10c10f66af17aa8a3ade8e704d06563aca2443e6
Nathan Sou <nathanksou@gmail.com>
2020-08-12 17:18:58 -0700

    Folder restructure and Home Components styled

--- education-app/src/App.tsx
+++ education-app/src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { AppDiv, AppHeader, Title, RotateAppLogo, AppLink } from './components/AppComponents';
-import logo from './logo.svg';
+import Home from './components/Home';
 
 function App() {
   return (
-    <AppDiv>
-      <AppHeader>
-        <RotateAppLogo src={logo} alt="logo"/>
-        <Title>Hello</Title>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-            Learn React
-          </AppLink>
-      </AppHeader>
-    </AppDiv>
+    <div>
+      <Home />
+    </div>
   );
 }
 
--- education-app/src/components/Home.jsx
+++ education-app/src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Button } from './styles/Components';
+import { HomePage } from './styles/HomeComponents';
 
-const Home = () => {
+const Home = (props) => {
 
+  return (
+    <HomePage>
+      <div>Hi there! Welcome to your education showcase.</div>
+      <div>
+        <div>
+          <label for="name">Type your name and click "Enter" below to begin!</label>
+        </div>
+        <div>
+          <input type="text" id="name" name="name" required
+        minlength="4"  size="30"></input>
+        </div>
+        <Button>Enter</Button>
+      </div>
+    </HomePage>
+  );
 };
 
 export default Home;
